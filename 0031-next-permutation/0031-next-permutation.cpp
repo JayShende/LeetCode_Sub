24902d006f21ebe34e0fdac6445d63b05d80680c
Jay Shende <85332904+JayShende@users.noreply.github.com>
2024-07-26 14:01:42 +0530

    Time: 8 ms (14.99%), Space: 14.8 MB (12.44%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,9 +1,41 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-      next_permutation(nums.begin(),nums.end());
-      return;
+    void nextPermutation(vector<int>& vec) {
+        
+        int i=-1;
 
-      // this Solution Uses an Inbuilt STL Lib next_permutation();
+        // Finding the Break point in the Curve
+        for(int a=vec.size()-1;a>0;a--)
+        {
+            if(vec[a]>vec[a-1])
+            {
+                i=a-1;
+                break;
+            }
+        }
+
+        if(i==-1)
+        {
+            reverse(vec.begin(),vec.end());
+            return;
+            // Handling an Edge Case ie when all Element are in Decending order ex vec={54321};
+            // then in Such Case the Next Permutation will be Reverse of All The Elements
+        }
+
+        // Finding an Element > vec[i] towards RHS of the i;
+
+        int j;
+        for(j=vec.size()-1;j>i;j--)
+        {
+            if(vec[j]>vec[i])
+            {
+                break;
+            }
+        }
+
+        swap(vec[i],vec[j]);
+
+        reverse(vec.begin()+i+1,vec.end());
+        return;
     }
 };
