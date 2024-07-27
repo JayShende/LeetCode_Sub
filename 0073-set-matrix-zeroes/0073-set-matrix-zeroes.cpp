65fa6522c948dabdb00445c517f5124ad279b9bd
Jay Shende <85332904+JayShende@users.noreply.github.com>
2024-07-28 00:00:37 +0530

    Time: 7 ms (83.18%), Space: 16.6 MB (47.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,33 +1,79 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& vec) {
-    // Better Solution
-    
-    
-    int n=vec.size(),m=vec[0].size();
-    vector<int> row(n,0),col(m,0);
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
+    void setZeroes(vector<vector<int>>& matrix) {
+      // Optimal Approach
+        int n=matrix.size(), m=matrix[0].size();
+        // Row - matrix[0][..]
+        // col matrix[..][0]
+        int col0=1;
+
+        // Step 1 Mark Yellow and Orange 
+
+        for(int i=0;i<n;i++)
         {
-            if(vec[i][j]==0)
+            for(int j=0;j<m;j++)
             {
-                row[i]=1;
-                col[j]=1;
+                if(matrix[i][j]==0)
+                {
+                    // Mark Row
+                    if(j==0)
+                    {
+                        col0=0;
+                    }
+                    else{
+                        matrix[0][j]=0;
+                    }
+
+                    // Mark Col
+                    matrix[i][0]=0;
+                }
+            }
+        }
+
+    // Set All Elements Excepy Yellow and Orange
+
+    for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<m;j++)
+        {
+            // Check Row and Col
+
+            if(matrix[0][j]==0 || matrix[i][0]==0)
+            {
+                matrix[i][j]=0;
             }
         }
     }
 
-    for(int i=0;i<n;i++)
+
+    // Set Yellow ie row
+
+    if(matrix[0][0]==0)
     {
-        for(int j=0;j<m;j++)
+        for(int j=1;j<m;j++)
         {
-            if(row[i]==1 || col[j]==1)
-            {
-                vec[i][j]=0;
-            }
+            matrix[0][j]=0;
         }
     }
+
+    // Set Orange ie Col
+
+    if(col0==0)
+    {
+        for(int i=0;i<n;i++)
+        {
+            matrix[i][0]=0;
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
     }
