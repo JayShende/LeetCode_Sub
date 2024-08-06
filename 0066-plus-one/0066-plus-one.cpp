ba78703ddbb638cd8d32f18e78dc289a6792ad5a
Jay Shende <85332904+JayShende@users.noreply.github.com>
2024-08-06 13:37:35 +0530

    Time: 0 ms (100%), Space: 10.6 MB (20.52%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,53 @@
+class Solution {
+private:
+    int fun(vector<int> vec)
+    {
+        int ans=0;
+        for(auto it:vec)
+        {
+            if(it==9)
+            {
+                ans++;
+            }
+        }
+        return (ans==vec.size());
+    }
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int c=1;
+        int n=digits.size();
+        if(fun(digits))
+        {
+            vector<int> ans;
+            ans.push_back(1);
+
+            for(int i=0;i<digits.size();i++)
+            {
+                ans.push_back(0);
+            }
+
+            return ans;
+        }
+       else if(c)
+       {    int i;
+            for(i=n-1;i>=0;i--)
+            {
+                if(digits[i]==9)
+                {
+                    digits[i]=0;
+                    c=1;
+                }
+                else{
+                    break;
+                }
+            }
+            digits[i]=digits[i]+c;
+            return digits;
+       }
+
+        
+        digits[n-1]=digits[n]+1;
+
+        return digits;
+    }
+};
