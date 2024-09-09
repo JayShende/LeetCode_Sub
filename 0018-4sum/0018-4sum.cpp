01b37b976636360969c81399ed4d2a18362016b5
Jay Shende <85332904+JayShende@users.noreply.github.com>
2024-09-09 13:12:07 +0530

    Time: 28 ms (53.26%), Space: 16.2 MB (40.77%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,32 +2,41 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n=nums.size();
-        
-        set<vector<int>> st2;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+
         for(int i=0;i<n;i++)
         {
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++)
-            {   
-                set<long long> st;
-                for(int k=j+1;k<n;k++)
-                {   
-                    long long sum=(long long)nums[i]+nums[j]+nums[k];
-                    long long l = (long long)target-sum;
+            { 
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1,l=n-1;
 
-                    if(st.find(l)!=st.end())
+                while(k<l)
+                {
+                    long long sum=(long long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum==target)
                     {
-                        vector<int> temp={nums[i],nums[j],nums[k],(int)l};
-                        sort(temp.begin(),temp.end());
-                        st2.insert(temp);
+                        vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                       ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }
+                    else if(sum<target)
+                    {
+                        k++;
                     }
                     else{
-                        st.insert(nums[k]);
+                        l--;
                     }
                 }
             }
         }
 
-        vector<vector<int>> ans(st2.begin(),st2.end());
+        // vector<vector<int>> ans(st.begin(),st.end());
         return ans;
     }
 };
