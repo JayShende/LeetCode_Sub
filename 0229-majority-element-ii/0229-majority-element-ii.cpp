d89776ded9fca4d9fd98c65fc25c3ed0ad47108b
Jay Shende <shendejay379@gmail.com>
2024-09-10 11:37:27 +0530

    Time: 7 ms (81.61%), Space: 18.6 MB (77.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,22 +1,47 @@
-// Better Approach Using Hashing
+//  Optimal Approach a Slight Modifiacation of Moore's Voting Algorithm
 
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        map<int,int> mpp;
+        int count1=0,count2=0;
+        int ele1,ele2;
+        
         vector<int> ans;
-        for(auto it:nums)
+
+        for(int i=0;i<n;i++)
         {
-            mpp[it]++;
+            if(count1==0 && ele2!=nums[i])
+            {
+                ele1=nums[i];
+                count1=1;
+            }
+            else if(count2==0 && ele1!=nums[i])
+            {
+                ele2=nums[i];
+                count2=1;
+            }
+            else if(nums[i]==ele1) count1++;
+            else if(nums[i]==ele2) count2++;
+            else{
+                count1--;
+                count2--;
+            }
         }
 
-        for(auto it:mpp)
+        int a=0,b=0;
+        for(auto it:nums)
         {
-            if(it.second>(n/3))
-            {
-                ans.push_back(it.first);
-            }
+            if(it==ele1) a++;
+            if(it==ele2) b++;
+        }
+        if(a>(n/3))
+        {
+            ans.push_back(ele1);
+        }
+        if(b>(n/3) && ele1!=ele2)
+        {
+            ans.push_back(ele2);
         }
 
         return ans;
