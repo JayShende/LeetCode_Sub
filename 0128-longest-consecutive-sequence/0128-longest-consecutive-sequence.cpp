04cb177ccd3573bd2e9be2c793ad82f930ee170b
Jay Shende <shendejay379@gmail.com>
2024-09-12 12:21:40 +0530

    Time: 68 ms (83.22%), Space: 49.7 MB (94.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,46 +1,27 @@
 class Solution {
-// Optimal Solution
-
-private:
-bool fun(unordered_set<int> st, int x)
-{
-    auto it=st.find(x);
-    if(it==st.end())
-    {
-        return false;
-    }
-    return true;
-}
 public:
+    // Better Solution The Brute Force Solution Hits TLE.
     int longestConsecutive(vector<int>& vec) {
-    int n=vec.size();
-    unordered_set<int> st;
-    int count=1,longest=0;
+        int n=vec.size();
+        sort(vec.begin(),vec.end());
 
-    for(auto it:vec)
-    {
-        st.insert(it);
-    }
+        int count=1, last_smallest=INT_MIN,largest=0;
 
-   for(auto it:st)
-    {   
-        int x=it;
-        if(st.find(x-1)==st.end())
-        {   //cout<<x-1<<"a"<<endl;
-            count=1;
-            while(st.find(x+1)!=st.end())
-            {   
-                // cout<<x+1<<"b"<<endl;
+        for(int i=0;i<n;i++)
+        {
+            if(vec[i]-1==last_smallest)
+            {
                 count++;
-                x++;
-                // cout<<count<<endl;
+                last_smallest=vec[i];
             }
+            else if(vec[i]!=last_smallest)
+            {
+                count=1;
+                last_smallest=vec[i];
+            }
+            largest=max(largest,count);
         }
 
-        longest=max(longest,count);
-    }
-
-    return longest;
-
+        return largest;
     }
 };
