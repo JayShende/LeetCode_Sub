bb8ad9f69dde1eef93f9c495da00c9d0157b362f
Jay Shende <shendejay379@gmail.com>
2024-09-23 13:11:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,38 +1,77 @@
 class Solution {
 private:
-    long long timeReq(vector<int> vec,int i)
+    int TimeReq(vector<int> vec, double x)
     {
-        long long ans=0;
+        int ans=0;
         for(auto it:vec)
         {
-            ans=ans+ceil((double)(it)/(double)(i));
+            ans=ans+ceil(it/x);
+        }
+
+        return ans;
+    }
+    int MaxElement(vector<int> vec)
+    {   
+        if(vec.size()==1) return vec[0];
+        int ans=INT_MIN;
+        for(auto it:vec)
+        {
+            if(it>ans)
+            {
+                ans=it;
+            }
         }
         return ans;
     }
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        // Binary Search Approach
-
-        int low=1,high=*max_element(piles.begin(),piles.end());
+int minEatingSpeed(vector<int> nums, int h) {
+        int low=1, high=MaxElement(nums);
         long long mid;
+        long long ans=INT_MAX;
 
         while(low<=high)
         {
             mid=low+(high-low)/2;
+            int a=TimeReq(nums,mid);
 
-            long long time=timeReq(piles,mid);
-
-            if(time<=h)
+            if(a<=h)
             {
-                // ie Koko has eatan Banana in time less or equal to h, 
-                // i want min walue of k so i move toward the left
-                high=mid-1;
-            } 
-            else{
+                // Can Be My Ans
+                ans=min(ans,mid);
+                //  i wanna inc time to reach h
+                // to inc time reduce deno (x)
+                // move to left search space
+                // eliminate the right one
+                 high=mid-1;
+            }
+            else
+            {
+                // Time Req > h 
+                // We Need To Reduce the Time Req --> inc deno ie mid
+                // To Increase Mid we Need to Move to the Right Sub Array
+                // Eliminate the Left Sub Array
                 low=mid+1;
             }
+            
         }
 
-        return low;
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
