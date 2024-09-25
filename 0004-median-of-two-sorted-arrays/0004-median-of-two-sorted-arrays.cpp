9c0b8bed2847dedfaf5f4b7b4ed6b58beebd28f9
Jay Shende <shendejay379@gmail.com>
2024-09-25 12:18:46 +0530

    Time: 34 ms (17.62%), Space: 94.8 MB (70.85%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,79 @@
 class Solution {
 public:
-   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-	vector<int> vec;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int n=n1+n2;
 
-	for (auto it : nums1)
-	{
-		vec.push_back(it);
-	}
-	for (auto it : nums2)
-	{
-		vec.push_back(it);
-	}
-	;
-	sort(vec.begin(), vec.end());
-	for (auto it : vec)
-	{
-		cout << it << " ";
-	}
-	cout << endl;
-	     int s = vec.size();
-	     cout<<s<<endl;
-	double ans = 0;
-	if (s % 2 == 0)
-	{
-		int n1 = vec[(s / 2)-1];
-		int n2 = vec[s / 2 ];
-		cout<<n1+n2<<endl;
-		ans = (n1 + n2) / 2.0;
+        int ind1=(n/2);
+        int ind2=(n/2)+1;
+        int ele,ele1,ele2;
+
+        int i=0,j=0;
+        int count=0;
+        while(i<n1 && j<n2)
+        {
+            if(nums1[i]<=nums2[j])
+            {
+                count++;
+                ele=nums1[i];
+                i++;
+            }
+            else{
+                ele=nums2[j];
+                j++;
+                count++;
+            }
+
+            if(count==ind1)
+            {
+                ele1=ele;
+            }
+            if(count==ind2)
+            {
+                ele2=ele;
+            }
+        }
+
+        while(i<n1)
+        {
+            ele=nums1[i];
+            count++;
+            i++;
+            if(count==ind1)
+            {
+                ele1=ele;
+            }
+            if(count==ind2)
+            {
+                ele2=ele;
+            }
+        }
+
+        while(j<n2)
+        {
+            ele=nums2[j];
+            count++;
+            j++;
+            if(count==ind1)
+            {
+                ele1=ele;
+            }
+            if(count==ind2)
+            {
+                ele2=ele;
+            }
+        }
+        double ans=0;
+        cout<<ele1<<" "<<ele2<<endl;
+        if(n%2==0)
+        {
+            ans=(double)(ele1+ele2)/2;
+        }
+        else{
+            ans=ele2;
+        }
+
         return ans;
-	}
-
-	ans = vec[s / 2] ;
-	return ans;
-   }
+    }
 };
