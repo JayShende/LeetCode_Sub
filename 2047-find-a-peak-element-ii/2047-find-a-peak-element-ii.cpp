0ea8c9c27a50c45204b156675c72dfd542e850b6
Jay Shende <shendejay379@gmail.com>
2024-10-07 11:35:51 +0530

    Time: 255 ms (5.7%), Space: 422.8 MB (5.6%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -1,24 +1,57 @@
 class Solution {
+private:
+    int fun(vector<vector<int>> vec, int i)
+    {   
+        int m=INT_MIN;
+        int ans=-1;
+        int n=vec[0].size();
+        for(int a=0;a<n;a++)
+        {
+            if(vec[i][a]>m)
+            {
+                ans=a;
+                m=vec[i][a];
+            }
+        }
+        return ans;
+    }
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        int a,b;
-        int ans=INT_MIN;
 
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            int j=fun(mat,i);
+            // mat[i][j] is the max ele among the row 
+            // ie no need to check for L and R
+            
+            int left,right;
+
+            // Check For top
+            if(i==0)
             {
-                if(mat[i][j]>ans)
-                {
-                    ans=mat[i][j];
-                    a=i; b=j;
-                }
+                left =-1;
+            }
+            else{
+                left =mat[i-1][j];
+            }
+
+            // Check for Bottom
+            if(i==(n-1))
+            {
+                right=-1;
+            }
+            else{
+                right=mat[i+1][j];
+            }
+            int ele=mat[i][j];
+            if(ele>left && ele > right)
+            {
+                return {i,j};
             }
         }
 
-        return {a,b};
-
+        return {-1,-1};
     }
 };
